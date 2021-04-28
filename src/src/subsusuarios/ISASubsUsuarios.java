ebdbc955e79ad75efe3a15ea7c444be8f9d9a59b
rodriss11 <57360904+rodriss11@users.noreply.github.com>
2021-04-28 19:13:41 +0200

    SubsUsuarios

---
+++ src/src/subsusuarios/ISASubsUsuarios.java
@@ -0,0 +1,20 @@
+package subsusuarios;
+
+import java.util.List;
+import usuariosdao.control.Persona;
+
+public interface ISASubsUsuarios {
+    boolean altaUsuario(Persona p);
+
+    boolean bajaUsuario(String dni);
+
+    List<Persona> consultarListaUsuarios(String nombre);
+
+    Persona buscarUsuario(String dni) throws Exception;
+
+    Persona modificarUsuario(Persona p);
+
+    boolean iniciarSesion(String dni, String contrasena);
+
+    boolean cerrarSesion();
+}
