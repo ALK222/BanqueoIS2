c5ce910d7ee65ca55d9fce7f320365666878c136
majir96 <jaiibane@ucm.es>
2021-05-05 11:32:02 +0200

    Commit correccion daocuentas

---
+++ src/src/cuentadao/control/IFachadaDAOCuentas.java
@@ -0,0 +1,17 @@
+package cuentadao.control;
+
+import java.util.List;
+
+public interface IFachadaDAOCuentas {
+    boolean altaCuenta(Cuenta c);
+
+    boolean bajaCuenta(int num_cuenta);
+
+    List<Cuenta> buscarListaCuentas(String titular_cuenta, String dni);
+
+    Cuenta consultarCuenta(int num_cuenta) throws Exception;
+
+    boolean modificarCuentas(Cuenta c);
+
+    JSONArray getMovimiento(int max_meses);
+}
