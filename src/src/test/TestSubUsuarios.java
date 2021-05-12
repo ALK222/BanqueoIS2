2279c294513664cccb21e50c414ff881fcf3e69e
Alejandro Barrachina <alejandrobarrachina.02@gmail.com>
2021-05-12 11:29:31 +0200

    testUsuario fix

--- src/src/test/TestSubUsuarios.java
+++ src/src/test/TestSubUsuarios.java
@@ -43,7 +43,7 @@ class TestSubUsuarios {
 			in = new FileInputStream(testFile);
 		} catch (FileNotFoundException e) {
 			try {
-				testFile = new File("resources\\usuarios.json");
+				testFile = new File("src/resources/usuarios.json"); 
 				in = new FileInputStream(testFile);
 			} catch (FileNotFoundException ex) {
 				throw ex;
