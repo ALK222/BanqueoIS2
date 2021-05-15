35b4b6c502b273220fce07a9ed5e32d542d5fc4d
Alejandro Barrachina <alejandrobarrachina.02@gmail.com>
2021-05-15 07:36:50 +0200

    file path fixed in usuarioJSON

--- src/src/usuariosdao/control/UsuariosJSON.java
+++ src/src/usuariosdao/control/UsuariosJSON.java
@@ -25,11 +25,11 @@ public class UsuariosJSON {
         File testFile = null;
         InputStream in = null;
         try {
-            testFile = new File("../resources/usuarios.json");
+            testFile = new File("src/resources/usuarios.json");
             in = new FileInputStream(testFile);
         } catch (FileNotFoundException e) {
             try {
-                testFile = new File("src/resources/usuarios.json");
+                testFile = new File("resources/usuarios.json");
                 in = new FileInputStream(testFile);
             } catch (FileNotFoundException ex) {
                 throw ex;
@@ -68,11 +68,11 @@ public class UsuariosJSON {
         File testFile = null;
         FileWriter in = null;
         try {
-            testFile = new File("../resources/usuarios.json");
+            testFile = new File("src/resources/usuarios.json");
             in = new FileWriter(testFile);
         } catch (FileNotFoundException e) {
             try {
-                testFile = new File("src/resources/usuarios.json");
+                testFile = new File("resources/usuarios.json");
                 in = new FileWriter(testFile);
             } catch (FileNotFoundException ex) {
                 throw ex;
